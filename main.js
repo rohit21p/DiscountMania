9dfd3dea6fe5b93b8c69b43ffe1c6c4185489d85
Rohit <iamrohit.panjwani@gmail.com>
2019-10-17 18:44:41 +0530

    allowed CORS

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("Incoming request");
+    res.setHeader('Access-Control-Allow-origin', '*');
     res.end();
 });
 
