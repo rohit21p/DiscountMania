eb8519ba67481eebf696037f45db3835c98b8133
Rohit <iamrohit.panjwani@gmail.com>
2019-10-17 23:41:30 +0530

    added sign-up endpoint

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const http = require('http');
 
+let body = [];
 const server = http.createServer((req, res) => {
     console.log("Incoming request");
     res.setHeader('Access-Control-Allow-origin', '*');
+    if(req.url=="/sign-up") {
+        parseBody(req);
+    }
     res.end();
 });
+
+function parseBody(req) {
+    req.on('data', (data) => {
+        body.push(data);
+    });
+    req.on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log(body);
+    });
+}
 
 server.listen(3000);
